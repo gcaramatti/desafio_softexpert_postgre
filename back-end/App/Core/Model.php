84bf126276dcfaecc4f92be84967c5e8b1a269f3
gcaramatti <biel.caramatti180@gmail.com>
2023-05-05 17:38:34 -0300

    Versão postgreSQL

--- back-end/App/Core/Model.php
+++ back-end/App/Core/Model.php
@@ -9,7 +9,7 @@ class Model {
     public static function getConn(){
 
         if(!isset(self::$connection)){
-            self::$connection = new \PDO("mysql:host=localhost;port=3306;dbname=desafio_softexpert;", "root", "root");
+            self::$connection = new \PDO("pgsql:host=localhost;port=5432;dbname=desafio_softexpert;", "postgres", "root");
         }
 
         return self::$connection;
